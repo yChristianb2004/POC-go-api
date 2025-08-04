da3d132a6d60bbfd71e9b3d2d565b157180f19a3
yChristianb2004 <Christianbsantos@outlook.com>
2025-08-04 05:20:08 -0300

    roles

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "os"
-    "api/models"
-    "api/routes"
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
-    "github.com/gin-gonic/gin"
+	"api/models"
+	"api/routes"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func InitDB() *gorm.DB {
-    dsn := os.Getenv("DATABASE_DSN")
-    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-    if err != nil {
-        panic("failed to connect database")
-    }
-    db.AutoMigrate(&models.User{})
-    return db
+	dsn := "host=localhost user=postgres password=123456 dbname=testdb port=5432 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database: " + err.Error())
+	}
+	db.AutoMigrate(&models.User{})
+	return db
 }
 
 func main() {
-    db := InitDB()
-    r := gin.Default()
-    routes.SetupRoutes(r, db)
-    r.Run()
+	db := InitDB()
+	r := gin.Default()
+	routes.SetupRoutes(r, db)
+	r.Run()
 }
